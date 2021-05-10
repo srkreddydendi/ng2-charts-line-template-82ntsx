d55abbcc78d12506638634311713e380b0a7563c
srkreddydendi <srkreddydendi@gmail.com>
2021-05-10 20:43:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng2-charts-line-template-82ntsx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng2-charts-line-template-82ntsx)
---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [10, 29, 34, 34, 55, 10, 20], label: 'Series A' },
+  ];
+  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,0,0,0.3)',
+    },
+    {
+      borderColor: 'green',
+      backgroundColor: 'rgba(222,22,0,0.3)',
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+  public lineChartPlugins = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
