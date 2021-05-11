254d81aa49fd88a4ff19aeb05db966b2687aaea2
srkreddydendi <srkreddydendi@gmail.com>
2021-05-11 10:26:55 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Color, Label } from 'ng2-charts';
 export class AppComponent  {
   public lineChartData: ChartDataSets[] = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [10, 29, 34, 34, 55, 10, 20], label: 'Series A' },
+    { data: [10, 29, 34, 34, 25, 10, 20], label: 'Series B' },
   ];
   public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions: (ChartOptions & { annotation: any }) = {
